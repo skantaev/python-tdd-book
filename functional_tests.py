c0eed3f5c8072cdd9bac8bad123cd340640ea084
Said Kantaev <sk@idaproject.com>
2020-05-17 12:58:02 +0300

    Multiple elements in table

--- functional_tests.py
+++ functional_tests.py
@@ -14,26 +14,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        # Переход по ссылка
         self.browser.get('http://localhost:8000')
 
+        # Проверка заголовка и тайтла
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
+        # Проверка на наличие инпутбокса
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
+        # Ввод текста
         inputbox.send_keys('Buy peacock feathers')
 
+        # Отправка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
+        # Проверка на наличие элемента
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            'New to-do item did not appear in table'
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        # Повторный ввод текста
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # Проверка на наличие элементов
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+
 
         self.fail('Finish the test!')
 
