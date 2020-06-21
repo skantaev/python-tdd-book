ae13eda8700f0b960c7b354cc4355407020f2b08
Said Kantaev <sk@idaproject.com>
2020-06-21 17:49:16 +0300

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -36,3 +36,22 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy milk')
         self.wait_for_row_in_list_table('2: Make tea')
+
+    def test_cannot_add_duplicate_items(self):
+        # Эдит отправляется на домашнюю страницу и создает новый список
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy wellies')
+
+        # Случайно она пытается ввести снова тот же элемент
+        self.get_item_input_box().send_keys('Buy wellies')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+
+        # Она видит полезное сообщение об ошибке
+        self.wait_for(
+            lambda: self.assertEqual(
+                self.browser.find_element_by_css_selector('.has-error').text,
+                "You've already got this in your list",
+            )
+        )
--- lists/models.py
+++ lists/models.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 
 
 class List(models.Model):
-
     def get_absolute_url(self):
         return reverse('view_list', args=[self.id])
 
@@ -11,3 +10,10 @@ class List(models.Model):
 class Item(models.Model):
     text = models.TextField(default='')
     list = models.ForeignKey(List, default=None)
+
+    class Meta:
+        ordering = ('id',)
+        unique_together = ('list', 'text')
+
+    def __str__(self):
+        return self.text
